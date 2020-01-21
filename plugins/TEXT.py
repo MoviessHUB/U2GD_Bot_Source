466c8e3c188de01af78a46b3c795c9232a83295b
sumitbot <botsumit@gmail.com>
2020-01-21 06:06:45 -0800

    Update TEXT.py

--- plugins/TEXT.py
+++ plugins/TEXT.py
@@ -5,7 +5,7 @@ drive_folder_name = "U2GD_bot"
 MEGA_EMAIL = "email"
 MEGA_PASSWORD = "password"
 
-START = "Hi {}  \nI am Upload 2 Drive Bot.\nPlease Authorise To use me.\nBy using /auth \n\nFor more info /help\n\nPlease Report Bugs @aryanvikash"
+START = "Hi {}  \nI am Upload 2 Drive Bot.\nPlease Authorise To use me.\nBy using /auth \n\nFor more info /help\n\nPlease Report Bugs @isumitbot"
 
 HELP = """<b>AUTHORISE BOT</b>
 Use  /auth Command Generate
@@ -30,19 +30,19 @@ DOWNLOAD = "Downloading Started ..."
 DOWN_MEGA = "Downloading Started... \nMega Links are \nExtremely Slow :("
 DOWN_COMPLETE = "Downloading complete!!"
 NOT_AUTH = "You Are Not Authorised To Using this Bot \n\nPlease Authorise Me Using /auth"
-REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise. \n\nreport At @aryanvikash "
+REVOKE_FAIL = "You Are Already UnAuthorised. \n\nPlease Use /auth To Authorise. \n\nreport At @isumitbot "
 AUTH_SUCC = "Authorised Successfully  !! \n\nNow Send me A direct Link :)"
-ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke \n\nreport At @aryanvikash "
+ALREADY_AUTH = "You Are Already Authorised!\n\nWanna Change Drive Account? \n\nUse /revoke \n\nreport At @isumitbot "
 AUTH_URL = '<a href ="{}">Click Here to Get Auth Code</a>\n\nGenerate And Copy Your Google Drive Token And Send It To Me'
 UPLOADING = "Download Complet !! \nUploading Your file"
 REVOKE_TOK = " Your Token is Revoked Successfully!! \n\nUse /auth To Re-Authorise Your Drive Acc."
 # DOWN_PATH = "Downloads\\" #windows path
 DOWN_PATH = "Downloads/"  # Linux path
 DOWNLOAD_URL = "Your File Uploaded Successfully.\n\n <b>Filename</b> : {} \n\n <b> Size</b> : {} MB \n\n <b>Download</b> {}"
-AUTH_ERROR = "AUTH Error!! Please Send Me a valid Token or Re-Authorise Me\n\n/auth\n\n report At @aryanvikash"
+AUTH_ERROR = "AUTH Error!! Please Send Me a valid Token or Re-Authorise Me\n\n/auth\n\n report At @isumitbot"
 OPENLOAD = True
 DROPBOX = True
 MEGA = True
 
 
-UPDATE = """Try @BhadooCloud"""
+UPDATE = """Try @skrul_bot"""
